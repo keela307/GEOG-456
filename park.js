3b8cabb45d9d9ecd8e46159da5819bc7881123bd
keela307 <keela@ad.unc.edu>
2026-01-22 23:34:53 -0500

    Homework 2

---
+++ park.js
@@ -0,0 +1,7 @@
+var park = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "descrip": "unc well" }, "geometry": { "type": "Point", "coordinates": [-81.47381444947317, 28.47212494473434] } }
+]
+}
